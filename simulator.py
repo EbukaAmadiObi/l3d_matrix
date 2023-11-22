250fffdc8f94679b813d0ef3e795c08580b85472
ebukaamadiobi <ebukaamadiobi@gmail.com>
2023-11-22 01:08:58 +0000

    refactor simulator functions
    write animation

--- simulator.py
+++ simulator.py
@@ -10,41 +10,118 @@ from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg,
 NavigationToolbar2Tk) 
 import matplotlib.pyplot as plt
 import numpy as np
+import csv
 
-# the main Tkinter window 
+# --------------------------------- Functions -------------------------------- #
+
+# Add a frame to the animation
+def add_frame():
+    global anim
+    global frames
+    # Create dot matrix
+    x_list = np.array([])
+    y_list = np.array([])
+    z_list = np.array([])
+    col = {}
+    for i in range(8):
+        for j in range(8):
+            for k in range(8):
+                x_list=np.append(x_list,i)
+                y_list=np.append(y_list,j)
+                z_list=np.append(z_list,k)
+                col[(i,j,k)] = "grey"
+    anim.append(col)
+    frames +=1
+    slider.configure(from_=0, to=frames-1)
+
+    current_frame = frames-1
+    slider.set(frames-1)
+    plot.cla()
+    draw_frame(anim[current_frame],plot)
+    canvas.draw()
+    
+
+# Draw frame on plot
+def draw_frame(frame,plot):
+    #variable to index through frame array to set light colours
+    h=0
+    x_list = np.array([])
+    y_list = np.array([])
+    z_list = np.array([])
+    for i in range(8):
+        for j in range(8):
+            for k in range(8):
+                x_list=np.append(x_list,i)
+                y_list=np.append(y_list,j)
+                z_list=np.append(z_list,k)
+
+    plot.scatter3D(x_list, y_list, z_list, color=[*frame.values()], alpha=0.75)
+
+def changeBrushRed():
+    global brush
+    brush = "red"
+
+def changeBrushGreen():
+    global brush
+    brush = "green"
+
+def changeBrushBlue():
+    global brush
+    brush = "blue"
+    
+# Function to change colour of dot
+def changeColour(frame, x1,y1,z1):
+    global light_buttons
+    global plot
+    if frame[(x1,y1,z1)] == brush:
+        frame[(x1,y1,z1)] = "grey"
+        light_buttons[x1][y1][z1].configure(bg="white")
+    else:
+        frame[(x1,y1,z1)] = brush
+        light_buttons[x1][y1][z1].configure(bg=brush)
+
+    plot.cla()
+    draw_frame(frame,plot)
+    canvas.draw()
+
+    print("x: ",x1," y: ",y1," z: ",z1," colour: ",frame[(x1,y1,z1)])
+
+def move_frame(event):
+    global current_frame
+    global slider
+    global plot
+    current_frame = slider.get()
+
+    plot.cla()
+    draw_frame(anim[current_frame],plot)
+    canvas.draw()
+
+# ----------------------------- Global Variables ----------------------------- #
+frames = 0
+current_frame = 0
+brush = "red"
+# Create buttons that change the colour of a dot
+light_buttons = [[[0 for x in range(8)] for x in range(8)] for x in range(8)]
+anim = []
+
+# ----------------------------------- Build UI ---------------------------------- #
+# The main Tkinter window 
 window = Tk() 
   
 # setting the title  
-window.title('Plotting in Tkinter') 
+window.title('L3D Cube Simulator') 
   
 # dimensions of the main window 
-window.geometry("600x600") 
+window.geometry("1000x600") 
 
 # Setting up plot
 fig = Figure(figsize=(5,5),dpi=100)
-
 plot = fig.add_subplot(111, projection='3d')
 plot.set_box_aspect([1,1,1])
 plot.set_title("Matrix", pad=25, size=15)
 plot.set_xlabel("X") 
 plot.set_ylabel("Y") 
 plot.set_zlabel("Z")
-
-# Create dot matrix
-x_list = np.array([])
-y_list = np.array([])
-z_list = np.array([])
-col = {}
-for i in range(8):
-    for j in range(8):
-        for k in range(8):
-            x_list=np.append(x_list,i)
-            y_list=np.append(y_list,j)
-            z_list=np.append(z_list,k)
-            col[(i,j,k)] = "grey"
-
-# Plotting
-plot.scatter3D(x_list, y_list, z_list, color=[*col.values()], alpha=0.75)
 
 # creating the Tkinter canvas 
 # containing the Matplotlib figure 
@@ -63,23 +140,17 @@ close_button = Button(master = window,
 
 close_button.grid(row=128,column=0,padx=10,pady=10)
 
-# Create buttons that change the colour of a dot
-light = [[[0 for x in range(8)] for x in range(8)] for x in range(8)]
+# draw frames slider
+slider = Scale(window, from_=0, to=frames-1, orient=HORIZONTAL,command = move_frame)
+slider.grid(row=18,column=2,columnspan=10)
 
-# Function to change colour of dot
-def changeColour(x1,y1,z1):
-    if col[(x1,y1,z1)] == "red":
-        col[(x1,y1,z1)] = "grey"
-        light[x1][y1][z1].configure(bg="grey")
-    else:
-        col[(x1,y1,z1)] = "red"
-        light[x1][y1][z1].configure(bg="red")
+#draw add frames button
+add_frame_font = font.Font(size=5)
+add_frame_button = Button(window, height = 10, width = 10, text = "Add Frame")
+add_frame_button.configure(command=add_frame)
+add_frame_button.grid(row=18,column=11)
 
-    plot.cla()
-    plot.scatter3D(x_list, y_list, z_list, color=[*col.values()], alpha=0.75)
-    canvas.draw()
-
-    print("x: ",x1," y: ",y1," z: ",z1," colour: ",col[(x1,y1,z1)])
+add_frame_button['font'] = add_frame_font
 
 # Create drawing buttons
 myFont = font.Font(size=1)
@@ -88,10 +159,10 @@ for z in range(8):
         for y in range(8):
 
             # Create button
-            light[x][y][z] = Button(window,bg="grey",height=5,width=3)
-
+            light_buttons[x][y][z] = Button(window,bg="white",height=5,width=3,bd=1)
+            
             # Configure button
-            light[x][y][z].configure(command=lambda x1=x, y1=y, z1=z: changeColour(x1,y1,z1))
+            light_buttons[x][y][z].configure(command=lambda x1=x, y1=y, z1=z: changeColour(anim[current_frame],x1,y1,z1))
 
             # Position button
             posx = x+(z*8)+1
@@ -115,9 +186,54 @@ for z in range(8):
                 posy = abs(y-7)+8
 
 
-            light[x][y][z].grid(column=posx,row=posy,padx=bufx,pady=bufy)
+            light_buttons[x][y][z].grid(column=posx,row=posy,padx=bufx,pady=bufy)
 
-            light[x][y][z]['font'] = myFont
+            light_buttons[x][y][z]['font'] = myFont
+
+# brush colour buttons
+brush_button_red = Button(master = window, height = 10, width = 10, text = "Red", bg ="red")
+brush_button_red.configure(command=changeBrushRed)
+brush_button_red.grid(row=17,column=1,columnspan=7,padx=10,pady=10)
+
+brush_button_green = Button(master = window, height = 10, width = 10, text = "Green", bg ="green")
+brush_button_green.configure(command=changeBrushGreen)
+brush_button_green.grid(row=17,column=8,columnspan=7,padx=10,pady=10)
+
+brush_button_blue = Button(master = window, height = 10, width = 10, text = "Blue", bg ="blue")
+brush_button_blue.configure(command=changeBrushBlue)
+brush_button_blue.grid(row=17,column=15,columnspan=7,padx=10,pady=10)
+
+# ------------------------------ Start Animation ----------------------------- #
+
+#Start blank animation
+add_frame()
+draw_frame(anim[0],plot)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 # run the gui 
 window.mainloop() 
--- test.py
+++ test.py
@@ -1,34 +1,40 @@
-# import all classes/methods 
-# from the tkinter module 
-from tkinter import *
+
+# Python program to demonstrate 
+# scale widget 
   
-# The main tkinter window 
-window = Tk() 
+from tkinter import * 
   
-# setting the title and  
-window.title('Plotting in Tkinter') 
   
-# setting the dimensions of  
-# the main window 
-window.geometry("500x500") 
+root = Tk()   
+root.geometry("400x300")  
+  
+v1 = DoubleVar() 
+  
+def show1():   
+      
+    sel = "Horizontal Scale Value = " + str(v1.get()) 
+    l1.config(text = sel, font =("Courier", 14))   
 
-light = [[0 for x in range(8)] for x in range(8)]
-
-def printcord(x,y,z):
-    print(x,y,z)
-
-for z in range(8):
-    for x in range(8):
-        for y in range(8):
-            light[x][y] = Button(window,bg="black")
-            light[x][y].configure(command=lambda x1=x, y1=y, z1=z: printcord(x1,y1,z1))
-            if x == 7:
-                light[x][y].grid(column=x+(z*8),row=abs(y-7),padx=(1,4),pady=1)
-            else:
-                light[x][y].grid(column=x+(z*8),row=abs(y-7),padx=1,pady=1)
-
-
-
-# run the gui 
-
-window.mainloop()
+def test(event):
+    print(s1.get())
+  
+  
+s1 = Scale( root, variable = v1,  
+           from_ = 1, to = 100,  
+           orient = HORIZONTAL,command = test)    
+  
+l3 = Label(root, text = "Horizontal Scaler") 
+  
+b1 = Button(root, text ="Display Horizontal",  
+            command = show1,  
+            bg = "yellow")   
+  
+l1 = Label(root) 
+  
+  
+s1.pack(anchor = CENTER)  
+l3.pack() 
+b1.pack(anchor = CENTER) 
+l1.pack()  
+  
+root.mainloop() 
